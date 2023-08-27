c0ff0bde74754c2f73e0673eabfadeb5a9cc432e
afu-hive <afu.sikarinp@hivecorps.com>
2023-08-27 20:22:39 +0700

    done Image Picker Page

--- app/_18ImagePicker.tsx
+++ app/_18ImagePicker.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
-import { View } from 'react-native'
+import { Linking, Text, TouchableOpacity, View } from 'react-native'
 import PageContainer from '@/layout/PageContainer'
 import ImagePicker from '@/components/ImagePicker'
 import { ImagePickerResult } from 'expo-image-picker'
+import Label from '@/components/Label'
+import CodeHighLighter from '@/components/CodeHighLighter'
+import { ACTIVE_OPACITY } from '@/components/Button'
 
 const _18ImagePicker = () => {
   const [images, setImages] = useState<any>([])
@@ -13,12 +16,39 @@ const _18ImagePicker = () => {
   return (
     <PageContainer>
       <View className="h-2" />
+      <Label bold>Basic</Label>
+      <View className="h-4" />
+      <CodeHighLighter language="typescript">
+        {`const [images, setImages] = useState<any>([])`}
+      </CodeHighLighter>
+      <View className="h-4" />
+      <CodeHighLighter language="typescript">
+        {`const onChangeImages = (images: ImagePickerResult) => {\n\tsetImages(images.assets)\n}`}
+      </CodeHighLighter>
+      <View className="h-4" />
+      <CodeHighLighter>
+        {`<ImagePicker\n\tvalues={images}\n\tonChange={onChangeImages}\n\tmultiple\n\tmaximum={6}\n\tlabel="Choose Images"\n\tbuttonLabel="Press for upload"\n/>`}
+      </CodeHighLighter>
+      <View className="h-4" />
+      <TouchableOpacity
+        onPress={() => Linking.openURL('https://docs.expo.dev/versions/latest/sdk/imagepicker')}
+        activeOpacity={ACTIVE_OPACITY}
+        className="px-2"
+      >
+        <Text className="text-lg font-bold text-blue-400">More Info About Expo Image Picker</Text>
+      </TouchableOpacity>
+      <View className="h-10" />
+      <View className="w-full h-[1px] bg-[#999]" />
+      <View className="h-14" />
       <ImagePicker
         values={images}
         onChange={onChangeImages}
         multiple
-        // editable
+        maximum={6}
+        label="Choose Images"
+        buttonLabel="Press for upload"
       />
+      <View className="h-10" />
     </PageContainer>
   )
 }
