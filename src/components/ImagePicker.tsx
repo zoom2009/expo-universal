fc1564a50e3e839ab9540fdfb37e5f144a647c57
afu-hive <afu.sikarinp@hivecorps.com>
2023-08-27 20:14:03 +0700

    done ImagePicker Component

--- src/components/ImagePicker.tsx
+++ src/components/ImagePicker.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, Image, Platform } from 'react-native'
+import { View, Text, Image, Platform, TouchableOpacity } from 'react-native'
+import * as R from 'ramda'
 import { Icon } from '@/components/Icon'
-import Button from '@/components/Button'
+import Button, { ACTIVE_OPACITY } from '@/components/Button'
 import { Layout } from '@/components/Layout'
 import theme from '@/utilities/theme'
 import MenuPicker from '@/components/MenuPicker'
@@ -14,9 +15,15 @@ interface IImagePickerProps {
   type?: 'All' | 'Images' | 'Videos'
   label?: string
   buttonLabel?: string
+  maximum?: number
   values: any[]
   onChange: (medias: ImagePickerModule.ImagePickerResult) => void
 }
+
+const menus = [
+  { text: 'Camera', Icon: <Icon.Camera weight="bold" size={28} color={theme.colors.primary} /> },
+  { text: 'Image', Icon: <Icon.Image weight="bold" size={28} color={theme.colors.primary} /> },
+]
 
 const ImagePicker = (props: IImagePickerProps) => {
   const {
@@ -28,6 +35,7 @@ const ImagePicker = (props: IImagePickerProps) => {
     type = 'Images',
     values,
     onChange,
+    maximum = 9999,
   } = props
 
   const [status, requestPermission] = ImagePickerModule.useCameraPermissions()
@@ -48,11 +56,14 @@ const ImagePicker = (props: IImagePickerProps) => {
     ImagePickerModule.launchImageLibraryAsync({
       mediaTypes: type as ImagePickerModule.MediaTypeOptions,
       allowsEditing: editable,
-      aspect,
       allowsMultipleSelection: multiple,
+      aspect,
     }).then((result) => {
       if (!result.canceled) {
-        onChange(result)
+        onChange({
+          canceled: false,
+          assets: R.slice(0, maximum, [...result.assets, ...values]),
+        })
       }
     }).catch((e) => {
       console.log('fail to pick image e:', e)
@@ -87,7 +98,7 @@ const ImagePicker = (props: IImagePickerProps) => {
 
   return (
     <>
-      <Layout.Wrapper>
+      <Layout.Wrapper className="items-start">
         <Layout._2_1>
           <View className="flex flex-col border-dashed border-2 border-info rounded-lg justify-center items-center px-5 py-6">
             <Icon.UploadSimple size={48} color={theme.colors.primary} />
@@ -105,17 +116,33 @@ const ImagePicker = (props: IImagePickerProps) => {
         </Layout._2_1>
         <Layout._2_1>
           <View className="h-2 sm:h-0" />
-          <View className="flex flex-1 flex-col border-dashed rounded-lg px-2 sm:px-4 pb-6 bg-red-100">
+          <View className="flex flex-1 flex-col border-dashed rounded-lg px-2 sm:px-4 pb-6">
             <View className="flex flex-row flex-wrap">
-              {values.map((v, index) => (
-                <View key={`${index}`} className="w-1/3 flex justify-center items-center">
-                  <Image
-                    resizeMode="cover"
-                    source={{ uri: v.uri }}
-                    className="w-[90%] h-[100px] rounded-md mt-4 self-center"
-                  />
-                </View>
-              ))}
+              {values.map((v, index) => {
+                const onRemove = () => {
+                  onChange({
+                    canceled: false,
+                    assets: R.remove(index, 1, values),
+                  })
+                }
+
+                return (
+                  <View key={`${index}`} className="w-1/3 flex justify-center items-center relative">
+                    <Image
+                      resizeMode="cover"
+                      source={{ uri: v.uri }}
+                      className="w-[90%] h-[100px] rounded-md mt-4 self-center"
+                    />
+                    <TouchableOpacity
+                      activeOpacity={ACTIVE_OPACITY}
+                      className="absolute -top-0 -right-0 z-10 w-[28px] h-[28px] flex justify-center items-center bg-danger rounded-full"
+                      onPress={onRemove}
+                    >
+                      <Icon.X weight="bold" size={14} color="white" />
+                    </TouchableOpacity>
+                  </View>
+                )
+              })}
             </View>
           </View>
         </Layout._2_1>
@@ -125,10 +152,7 @@ const ImagePicker = (props: IImagePickerProps) => {
         visible={isShowImageAndCameraMenu}
         onClose={onCloseImageAndCameraMenu}
         onSelect={onSelectImageAndCameraMenu}
-        menus={[
-          { text: 'Camera', Icon: <Icon.Camera weight="bold" size={28} color={theme.colors.primary} /> },
-          { text: 'Image', Icon: <Icon.Image weight="bold" size={28} color={theme.colors.primary} /> },
-        ]}
+        menus={menus}
       />
     </>
   )
